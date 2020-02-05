1b0ac711379a9a46f29be0d6e905288311126354
ryo0916 <rkganma@gmail.com>
2020-02-05 23:11:25 +0900

    signup validation

--- goodcareer-functions/functions/index.js
+++ goodcareer-functions/functions/index.js
@@ -55,6 +55,17 @@ app.post('/company', (req, res) => {
     });
 });
 
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(email.match(regEx)) return true;
+  else return false;
+}
+
+const isEmpty = (string) => {
+  if(string.trim() === '') return true;
+  else return false;
+}
+
 // SIGNUP route
 app.post('/signup', (req, res) => {
   const newUser = {
@@ -63,6 +74,20 @@ app.post('/signup', (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle
   };
+
+  let errors = {};
+
+  if(isEmpty(newUser.email)) {
+    errors.email = 'Email must not be empty'
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address'
+  }
+
+  if(isEmpty(newUser.password)) errors.password = 'Must not be empty';
+  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+  if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+  if(Object.keys(errors).length > 0) return res.status(400).json(errors)
 
   let token, userId;
   db.doc(`/users/${newUser.handle}`).get()
