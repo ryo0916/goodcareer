8c602cdafb5a20fa012c762b292c41f5608b72bb
ryo0916 <rkganma@gmail.com>
2020-02-23 00:01:17 +0900

    Muithemeprovider

--- goodcareer-app/src/App.js
+++ goodcareer-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // Components
 import Navbar from './components/Navbar';
@@ -10,9 +12,27 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  }
+})
+
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
       <Router>
         <Navbar />
         <div className="container">
@@ -24,6 +44,7 @@ function App() {
         </div>
       </Router>
     </div>
+    </MuiThemeProvider>
   );
 }
 
