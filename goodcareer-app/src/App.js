583b77ccc1910b31a115828eaa369d5d1e3c1c11
ryo0916 <rkganma@gmail.com>
2020-02-22 23:54:50 +0900

    react-css

--- goodcareer-app/src/App.js
+++ goodcareer-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css'
 
 // Components
 import Navbar from './components/Navbar';
@@ -14,11 +15,13 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
+        <div className="container">
         <Switch>
-          <Route exact path="/" component={home}/>
-          <Route exact path="/login" component={login}/>
-          <Route exact path="/signup" component={signup}/>
+          <Route exact path="/" component={home} />
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
         </Switch>
+        </div>
       </Router>
     </div>
   );
