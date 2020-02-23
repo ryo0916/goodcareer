d284ca4cd01bca1d54ac28fc0d3218851f6b49bb
ryo0916 <rkganma@gmail.com>
2020-02-24 01:17:00 +0900

    card for company

--- goodcareer-app/src/pages/home.js
+++ goodcareer-app/src/pages/home.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
-
+import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 
+import Company from '../components/Company';
+
 class home extends Component {
+  state = {
+    companies: null
+  }
+  componentDidMount(){
+    axios.get('/companies')
+      .then((res) => {
+        this.setState({
+          companies: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
+    let recentCompaniesMarkup = this.state.companies ? (
+    this.state.companies.map((company) => <Company key={company.companyId} company={company} />)
+    ) : (
+      <p>Loading...</p>
+    );
     return (
-      <Grid container spacing={16}>
+      <Grid container spacing={4}>
         <Grid item sm={8} xs={12}>
-          <p>Content....</p>
+          {recentCompaniesMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
           <p>Profile....</p>
