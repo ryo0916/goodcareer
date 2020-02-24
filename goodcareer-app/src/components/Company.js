222f5d356dae622b4709e7ba425525672ed9fa06
ryo0916 <rkganma@gmail.com>
2020-02-24 23:37:46 +0900

    loginページ修正

--- goodcareer-app/src/components/Company.js
+++ goodcareer-app/src/components/Company.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 // MUI Stuff
 import Card from '@material-ui/core/Card';
@@ -24,6 +26,7 @@ const styles = {
 
 class Company extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const { classes, company: { body, createdAt, userImage, userHandle, companyId} } = this.props;
     return (
       <Card className={classes.card} >
@@ -33,7 +36,7 @@ class Company extends Component {
           className={classes.image} />
         <CardContent className={classes.content}>
         <Typography variant="h5" component={Link} to={`/users/${userHandle}`} color="primary">{userHandle}</Typography>
-        <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+        <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
         <Typography variant="body1">{body}</Typography>
         </CardContent>
       </Card>
